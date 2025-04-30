9a93b4e5566e36eb8396fd8f6acd5db2317e3b2c
Danny Armijos <daniel2667@hotmail.es>
2025-04-30 19:39:29 +0200

    feat: refactor calculator buttons into separate component for better modularity

---
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'calculator-button',
+  imports: [],
+  templateUrl: './calculator-button.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    class: 'w-1/4 border-r border-b border-indigo-400',
+    // attribute: 'role',
+    // value: 'button',
+    // 'aria-label': 'calculator button',
+  },
+})
+export class CalculatorButtonComponent {}
